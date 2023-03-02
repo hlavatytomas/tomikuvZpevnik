fb044905c95b05ffc7cbc948bba6977d0d8da516
Jan Hrůza <hruza.jan1@gmail.com>
2023-03-02 23:46:41 +0100

    Side menu

--- django/docs/menu.js
+++ django/docs/menu.js
@@ -1,20 +1,17 @@
 var open = document.getElementById("open_pdf_menu");
-open.addEventListener("click", function(){
+open.addEventListener("click", function(event){
     document.getElementById("sidebar").classList.toggle("active");
     document.getElementById("hide").classList.toggle("active");
-    event.stopPropagation();
 })
 
 var close = document.getElementById("close_pdf_menu");
-close.addEventListener("click", function(){
+close.addEventListener("click", function(event){
     document.getElementById("sidebar").classList.toggle("active");
     document.getElementById("hide").classList.toggle("active");
-    event.stopPropagation();
 })
 
 var hide = document.getElementById("hide");
-close.addEventListener("click", function(){
+hide.addEventListener("click", function(event){
     document.getElementById("sidebar").classList.toggle("active");
     document.getElementById("hide").classList.toggle("active");
-    event.stopPropagation();
 })
